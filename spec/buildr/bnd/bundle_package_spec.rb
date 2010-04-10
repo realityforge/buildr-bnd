389af89bdeaebbf1b83c603325becdc540ae8b54
Peter Donald <peter@realityforge.org>
2010-04-10 17:43:08 +1000

    Add more tests for subpackage

--- spec/buildr/bnd/bundle_package_spec.rb
+++ spec/buildr/bnd/bundle_package_spec.rb
@@ -58,17 +58,47 @@ SRC
     end
   end
 
+  it "produces a .bnd in the correct location for subproject project" do
+    File.should be_exist(@foo._("bar/target/foo-bar-2.2.bnd"))
+  end
 
-  def open_zip_file
-    jar_filename = @foo._("target/foo-2.1.3.jar")
+  it "produces a .jar in the correct location for subproject project" do
+    File.should be_exist(@foo._("bar/target/foo-bar-2.2.jar"))
+  end
+
+  it "produces a .jar containing correct .class files for subproject project" do
+    open_zip_file('bar/target/foo-bar-2.2.jar') do |zip|
+      zip.file.exist?('com/biz/bar/Bar.class').should be_true
+    end
+  end
+
+  it "produces a .jar containing expected manifest entries derived from project.bnd for subproject project" do
+    open_main_manifest_section('bar/target/foo-bar-2.2.jar') do |attribs|
+      attribs['Bundle-Name'].should eql('foo:bar')
+      attribs['Bundle-Version'].should eql('2.2')
+      attribs['Bundle-SymbolicName'].should eql('mygroup.foo-bar')
+      attribs['Export-Package'].should eql('com.biz.bar')
+      attribs['Import-Package'].should eql('com.biz.bar')
+    end
+  end
+
+  it "produces a .jar containing expected manifest entries derived from project.manifest subproject project" do
+    open_main_manifest_section('bar/target/foo-bar-2.2.jar') do |attribs|
+      attribs['Magic-Drink'].should eql('Wine')
+      attribs['Magic-Food'].should eql('Cheese')
+    end
+  end
+
+  def open_zip_file(file = 'target/foo-2.1.3.jar')
+    jar_filename = @foo._(file)
     File.should be_exist(jar_filename)
     Zip::ZipFile.open(jar_filename) do |zip|
       yield zip
     end
   end
 
-  def open_main_manifest_section
-    jar_filename = @foo._("target/foo-2.1.3.jar")
+  def open_main_manifest_section(file = 'target/foo-2.1.3.jar')
+    jar_filename = @foo._(file)
     File.should be_exist(jar_filename)
     yield Buildr::Packaging::Java::Manifest.from_zip(jar_filename).main
   end
