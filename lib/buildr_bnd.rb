5a5a24032b6b148a84504542924b609811c87638
Peter Donald <peter@realityforge.org>
2010-07-05 21:15:00 +1000

    Extract BundleTask class into a separate file

--- lib/buildr_bnd.rb
+++ lib/buildr_bnd.rb
@@ -1,2 +1,3 @@
 require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/version')
+require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/bundle_task')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/project_extension')
