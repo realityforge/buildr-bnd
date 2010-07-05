f66a2761d77e2d9b1c762e5f214497fd8f3544d2
Peter Donald <peter@realityforge.org>
2010-07-05 21:16:37 +1000

    Extract Bnd class methods into a separate file

---
+++ lib/buildr/bnd/main.rb
@@ -0,0 +1,26 @@
+module Buildr
+  module Bnd
+    class << self
+      # The specs for requirements
+      def requires
+        ["biz.aQute:bnd:jar:0.0.384"]
+      end
+
+      # Repositories containing the requirements
+      def remote_repositories
+        puts "Buildr::Bnd.remote_repositories is deprecated. Please use Buildr::Bnd.remote_repository instead." 
+        [remote_repository]
+      end
+
+      # Repositories containing the requirements
+      def remote_repository
+        "http://www.aQute.biz/repo"
+      end
+
+      def bnd_main(*args)
+        cp = Buildr.artifacts(self.requires).each(&:invoke).map(&:to_s).join(File::PATH_SEPARATOR)
+        Java::Commands.java 'aQute.bnd.main.bnd', *(args + [{ :classpath => cp }])
+      end
+    end
+  end
+end
--- lib/buildr_bnd.rb
+++ lib/buildr_bnd.rb
@@ -1,3 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/version')
+require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/main')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/bundle_task')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/bnd/project_extension')
