ad1c1ecc882b177eb8a8f917ea208be1a30dce06
Peter Donald <peter@realityforge.org>
2010-07-05 21:19:32 +1000

    Refactor extension so that it looks like the extension in other projects

--- lib/buildr/bnd/project_extension.rb
+++ lib/buildr/bnd/project_extension.rb
@@ -1,59 +1,36 @@
 module Buildr
   module Bnd
-    include Buildr::Extension
+    module ProjectExtension
+      include Extension
 
-    class << self
-
-      # The specs for requirements
-      def requires
-        ["biz.aQute:bnd:jar:0.0.384"]
+      first_time do
+        desc "Does `bnd print` on the packaged bundle and stdouts the output for inspection"
+        Project.local_task("bnd:print")
       end
 
-      # Repositories containing the requirements
-      def remote_repositories
-        puts "Buildr::Bnd.remote_repositories is deprecated. Please use Buildr::Bnd.remote_repository instead." 
-        [remote_repository]
+      def package_as_bundle(filename)
+        project.task('bnd:print' => [filename]) do |task|
+          Buildr::Bnd.bnd_main("print", filename)
+        end
+
+        dirname = File.dirname(filename)
+        directory(dirname)
+
+        # Add Buildr.application.buildfile so it will rebuild if we change settings
+        task = BundleTask.define_task(filename => [Buildr.application.buildfile, dirname])
+        task.project = self
+        # the last task is the task considered the packaging task
+        task
       end
 
-      # Repositories containing the requirements
-      def remote_repository
-        "http://www.aQute.biz/repo"
+      def package_as_bundle_spec(spec)
+        # Change the source distribution to .jar extension
+        spec.merge(:type => :jar)
       end
-
-      def bnd_main(*args)
-        cp = Buildr.artifacts(self.requires).each(&:invoke).map(&:to_s).join(File::PATH_SEPARATOR)
-        Java::Commands.java 'aQute.bnd.main.bnd', *(args + [{ :classpath => cp }])
-      end
-
-    end
-
-    def package_as_bundle(filename)
-      project.task('bnd:print' => [filename]) do |task|
-        Bnd.bnd_main( "print", filename )
-      end
-
-      dirname = File.dirname(filename)
-      directory( dirname )
-
-      # Add Buildr.application.buildfile so it will rebuild if we change settings
-      task = BundleTask.define_task(filename => [Buildr.application.buildfile, dirname])
-      task.project = self
-      # the last task is the task considered the packaging task
-      task
-    end
-
-    def package_as_bundle_spec(spec)
-      # Change the source distribution to .jar extension
-      spec.merge( :type => :jar )
-    end
-
-    first_time do
-      desc "Does `bnd print` on the packaged bundle and stdouts the output for inspection"
-      Project.local_task("bnd:print")
     end
   end
 end
 
 class Buildr::Project
-  include Buildr::Bnd
+  include Buildr::Bnd::ProjectExtension
 end
