dafd7a561143c3bc6dbe166c534fda8a3fb93929
Peter Donald <peter@realityforge.org>
2010-08-22 13:53:12 +1000

    Remove unecessary guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,47 +8,45 @@ unless File.exist?("#{BUILDR_DIR}/buildr.gemspec")
 end
 
 # hook into buildr's spec_helpers load process
-unless defined?(SpecHelpers)
-  module SandboxHook
-    def SandboxHook.included(spec_helpers)
-      $LOAD_PATH.unshift(File.dirname(__FILE__))
-      $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-      require 'buildr_bnd'
-    end
+module SandboxHook
+  def SandboxHook.included(spec_helpers)
+    $LOAD_PATH.unshift(File.dirname(__FILE__))
+    $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+    require 'buildr_bnd'
   end
+end
 
-  require "#{BUILDR_DIR}/spec/spec_helpers.rb"
+require "#{BUILDR_DIR}/spec/spec_helpers.rb"
 
-  # Download deps into real local dir
-  Buildr::repositories.remote << Buildr::Bnd.remote_repository
-  Buildr::Bnd.requires.each { |spec| artifact(spec).invoke }
+# Download deps into real local dir
+Buildr::repositories.remote << Buildr::Bnd.remote_repository
+Buildr::Bnd.requires.each { |spec| artifact(spec).invoke }
 
-  # Adjust specs so that they do not attempt to constantly download helper artifacts
-  module BuildrBndSpecHelpers
+# Adjust specs so that they do not attempt to constantly download helper artifacts
+module BuildrBndSpecHelpers
 
-    HELPERS_REPOSITORY = "file://#{Buildr::repositories.local}"
-    LOCAL_TEST_REPOSITORY = File.expand_path File.join(File.dirname(__FILE__), "..", "tmp", "test_m2_repository")
+  HELPERS_REPOSITORY = "file://#{Buildr::repositories.local}"
+  LOCAL_TEST_REPOSITORY = File.expand_path File.join(File.dirname(__FILE__), "..", "tmp", "test_m2_repository")
 
-    class << self
+  class << self
 
-      def included(config)
-        config.before(:each) do
-          repositories.remote << "file://#{HELPERS_REPOSITORY}"
-        end
-        config.after(:all) do
-          FileUtils.rm_rf LOCAL_TEST_REPOSITORY
-        end
+    def included(config)
+      config.before(:each) do
+        repositories.remote << "file://#{HELPERS_REPOSITORY}"
+      end
+      config.after(:all) do
+        FileUtils.rm_rf LOCAL_TEST_REPOSITORY
       end
     end
-
-    def createRepository(name)
-      repo = File.join(LOCAL_TEST_REPOSITORY, name)
-      mkpath repo
-      return repo
-    end
   end
 
-  Spec::Runner.configure do |config|
-    config.include BuildrBndSpecHelpers
+  def createRepository(name)
+    repo = File.join(LOCAL_TEST_REPOSITORY, name)
+    mkpath repo
+    return repo
   end
+end
+
+Spec::Runner.configure do |config|
+  config.include BuildrBndSpecHelpers
 end
